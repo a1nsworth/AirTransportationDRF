fd13968d35fcb1f7ae1f9d5d1fecae767a5765ef
a1nsworth <xed255@gmail.com>
2023-12-25 19:49:42 +0300

    setup

--- air_transportation/settings.py
+++ air_transportation/settings.py
@@ -63,10 +63,10 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = "*"
-CSRF_TRUSTED_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = "air_transportation.urls"
 
